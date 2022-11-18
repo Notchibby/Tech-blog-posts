38e88585ce0ca0c59dadb22c44fcf3384c72261e
Chibuike Okoye~ <talk2chibyke@yahoo.com>
2022-11-18 23:37:04 +1100

    finished writing the seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,22 +1,24 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  for (const blog of blogData) {
+    await Blog.create(blog);
+  }
+
+  for (const comment of commentData){
+    await Comment.create(comment);
   }
 
   process.exit(0);
